79ce1658402477504fa897143758edee324d756b
Pavel Iliev <35112204+SonicTheCat@users.noreply.github.com>
2018-05-18 04:07:30 +0300

    exercise three added
    exercise three added

--- 03.ExpressionsCondStatLoops/03.DistanceOverTime.js
+++ 03.ExpressionsCondStatLoops/03.DistanceOverTime.js
@@ -1,11 +1,9 @@
 function distanceObjects(input) {
-
-        let dist1= (input[0] / 3.6) * input[2];
-        console.log(dist1);
-        let dist2= (input[1] / 3.6) * input[2];
-        console.log(dist2);
-        let delta = Math.abs(dist1- dist2);
-        console.log(delta);
+        let dist1= Number(input[0] * 1000);
+        let dist2= Number(input[1] * 1000);
+        let time = Number(input[2]/3600);
+        let result = Math.abs(dist1 * time - dist2 * time);
+        console.log(result);
 }
 
 distanceObjects([11,10,120]);
